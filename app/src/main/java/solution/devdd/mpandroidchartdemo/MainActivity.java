5179579c194cd4ab53a7e6c02b5cc62ccca664fc
Ronnapee <j.lnw.ronnapee@gmail.com>
2018-05-31 21:16:07 +0700

    piechart + animation

--- app/src/main/java/solution/devdd/mpandroidchartdemo/MainActivity.java
+++ app/src/main/java/solution/devdd/mpandroidchartdemo/MainActivity.java
@@ -11,19 +11,25 @@ import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
 import android.util.Log;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -46,16 +52,39 @@ public class MainActivity extends AppCompatActivity {
             entries.add(new PieEntry(student.getScore(), student.getName()));
         }
 
-        PieDataSet dataset = new PieDataSet(entries, "Student");
+        PieDataSet dataset = new PieDataSet(entries, "student");
         dataset.setSelectionShift(10);
         dataset.setValueTextSize(14);
-        dataset.setColors(ColorTemplate.MATERIAL_COLORS);
+        dataset.setColors(ColorTemplate.PASTEL_COLORS); // set the color
+        // dataset.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataset.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataset.setValueLinePart1Length(0.5f);
+        dataset.setValueLinePart2Length(0.5f);
+        dataset.setValueFormatter(new PercentFormatter());
 
-        PieData data = new PieData(dataset);
-
+        PieData data = new PieData(dataset); // initialize Piedata
+//      data.setValueFormatter(new PercentFormatter());
+        chart.setNoDataText("No data available.");
         chart.setData(data);
-
         chart.setHoleRadius(30);
         chart.setTransparentCircleRadius(40);
+        //chart.setDescription(#);  // set the description
+        //chart.animateY(3000);
+        chart.animateY(3000, Easing.EasingOption.EaseOutBounce);
+//        chart.setUsePercentValues(true);
+        chart.setEntryLabelTextSize(14);
+        chart.setEntryLabelColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimaryDark));
+
+        chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                // when select data.
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
     }
 }
